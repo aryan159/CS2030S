b5baf27ea34a770ad08c5938a77c79fde1a6f46f
ARYAN JAIN <aryan159@comp.nus.edu.sg>
2022-11-14 23:16:41 +0800

    Lab9b Solved

--- Lab9/Lab9b/Streaming.java
+++ Lab9/Lab9b/Streaming.java
@@ -5,6 +5,34 @@ import java.util.ArrayList;
 
 public class Streaming {
   public static <T> List<Pair<Integer, T>> encode(Stream<T> stream) {
-    return null; // Modify this
+    return stream
+      .reduce(
+        new ArrayList<Pair<Integer, T>>(),
+        (arr, x) -> {
+          ArrayList<Pair<Integer, T>> newArr = new ArrayList<Pair<Integer, T>>();
+          newArr.addAll(arr);
+          if (!newArr.isEmpty() && newArr.get(newArr.size() - 1).getSnd() == x) {
+            Pair<Integer, T> cur = newArr.get(newArr.size() - 1);
+            cur.setFst(cur.getFst() + 1);
+          }
+          else {
+            newArr.add(new Pair<Integer, T>(1, x));
+          }
+          return newArr;
+        },
+        (arr1, arr2) -> {
+          if (!arr1.isEmpty() 
+              && !arr2.isEmpty()
+              && arr1.get(arr1.size() - 1).getSnd() == arr2.get(0).getSnd()
+              ) {
+            arr2.get(0).setFst(
+                arr2.get(0).getFst()
+                + arr1.get(arr1.size() - 1).getFst());
+            arr1.remove(arr1.size() - 1);
+          }
+          arr1.addAll(arr2);
+          return arr1;
+        }
+      );
   }
 }
